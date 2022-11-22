d1d186318919763f91c8aca8fca8a2a0a98a48a9
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-11-21 22:12:43 -0300

    2.21. Criando métodos de callback do ciclo de vida dos beans com anotações @PostConstruct e @PreDestroy

--- src/main/java/com/feernandoalencar/curso_esr/di/service/AtivacaoClienteService.java
+++ src/main/java/com/feernandoalencar/curso_esr/di/service/AtivacaoClienteService.java
@@ -7,6 +7,9 @@ import com.feernandoalencar.curso_esr.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class AtivacaoClienteService {
 
@@ -14,6 +17,15 @@ public class AtivacaoClienteService {
     @Autowired
     private Notificador notificador;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("INIT " + notificador);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();

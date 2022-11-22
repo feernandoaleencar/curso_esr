4576462bbc460a39dfa789c0a8868a8d4d454936
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-11-21 22:18:56 -0300

    2.21. Criando métodos de callback do ciclo de vida dos beans com initMethod e destroyMethod

---
+++ src/main/java/com/feernandoalencar/curso_esr/di/config/ServiceConfig.java
@@ -0,0 +1,14 @@
+package com.feernandoalencar.curso_esr.di.config;
+
+import com.feernandoalencar.curso_esr.di.service.AtivacaoClienteService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public AtivacaoClienteService ativacaoClienteService(){
+        return new AtivacaoClienteService();
+    }
+}
--- src/main/java/com/feernandoalencar/curso_esr/di/service/AtivacaoClienteService.java
+++ src/main/java/com/feernandoalencar/curso_esr/di/service/AtivacaoClienteService.java
@@ -10,19 +10,19 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Component
+//@Component
 public class AtivacaoClienteService {
 
     @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
     private Notificador notificador;
 
-    @PostConstruct
+    //@PostConstruct
     public void init() {
         System.out.println("INIT " + notificador);
     }
 
-    @PreDestroy
+    //@PreDestroy
     public void destroy() {
         System.out.println("DESTROY");
     }

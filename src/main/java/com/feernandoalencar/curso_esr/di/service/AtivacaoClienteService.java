0e832998ae7eecb405ffb847eb5f6e17da5b73dd
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-11-21 21:38:20 -0300

    2.20. Mudando o comportamento da aplicação com Spring Profiles

--- src/main/java/com/feernandoalencar/curso_esr/di/service/AtivacaoClienteService.java
+++ src/main/java/com/feernandoalencar/curso_esr/di/service/AtivacaoClienteService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AtivacaoClienteService {
 
-    @TipoDoNotificador(NivelUrgencia.URGENTE)
+    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
     private Notificador notificador;
 
